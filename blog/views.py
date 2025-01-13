690084d0607e93c4fddbeb2cae4d942041852c99
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-13 20:10:16 +0530

    new

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse # type: ignore
 # Create your views here.
 def index(request):
     return HttpResponse("Hi, You are at blog's index")
+
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing post detail page. And ID is {post_id}")
