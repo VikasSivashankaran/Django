99de8044ebe4287c0e274ca44db46ef871948090
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-13 21:15:35 +0530

    new

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index,name = "index"),
     #path("post/detail",views.detail,name="detail")
-    path("post/<int:post_id>",views.detail, name="detail")
+    path("post/<int:post_id>",views.detail, name="detail"),
+    path("new_url",views.new_url_redirect, name="new_url"),
+    path("old_url",views.old_url_redirect, name="old_url")
 ]
