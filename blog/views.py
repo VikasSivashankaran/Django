7a00f43b7d1d6a684a02d70379443a589565946c
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-18 20:46:48 +0530

    for tag

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,13 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     blog_title = "New Feeds"
-    return render(request,'index.html',{'blog_title':blog_title})   #variable interpolation
+    posts = [
+        {'title':'Post 1', 'content':'Content of Post 1'},
+        {'title':'Post 2', 'content':'Content of Post 2'},
+        {'title':'Post 3', 'content':'Content of Post 3'},
+        {'title':'Post 4', 'content':'Content of Post 4'},
+    ]
+    return render(request,'index.html',{'blog_title':blog_title, 'posts':posts })   #variable interpolation
 
 def detail(request, post_id):
     return render(request,'detail.html')
