6bb964f48703d51f2105e1344606e319473a76ad
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-20 10:01:49 +0530

    custom 404 page

--- Project_Django/settings.py
+++ Project_Django/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-izn2^0%73a!&)t--=ut9!+9q!=-nolr(_fi=nbf5ll4#ujr6cn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '120.0.0.1'
+]
 
 
 # Application definition
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'Project_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
