199260defc54c54f1734fa75e28632035644ab9a
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-21 17:42:42 +0530

    new

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect # type: ignore
 from django.http import HttpResponse # type: ignore
 from django.urls import reverse
+import logging
 # Create your views here.
-def index(request):
-    blog_title = "New Feeds"
-    posts = [
+
+
+posts = [
         {'id':1,'title':'Post 1', 'content':'Content of Post 1'},
         {'id':2,'title':'Post 2', 'content':'Content of Post 2'},
         {'id':3,'title':'Post 3', 'content':'Content of Post 3'},
         {'id':4,'title':'Post 4', 'content':'Content of Post 4'},
     ]
+
+def index(request):
+    blog_title = "New Feeds"
     return render(request,'index.html',{'blog_title':blog_title, 'posts':posts })   #variable interpolation
 
 def detail(request, post_id):
-    return render(request,'detail.html')
+    post = next((item for item in posts if item['id'] == int(post_id)), None)
+    logger = logging.getLogger("TESTING")
+    logger.debug(f'post vasriable is {post}')
+    return render(request,'detail.html',{'post':post})
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
