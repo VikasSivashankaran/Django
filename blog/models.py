25565910bfc6649f5f3c581e6489e56370fcbc15
VikasSivashankaran <vikasbmca@gmail.com>
2025-01-22 10:04:46 +0530

    migrations

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     img_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -16,4 +26,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-    
+
